3ba1996d0e3050262cd0215cb130fd68e92e47cd
LuísCosta <luistelmocosta@gmail.com>
2018-02-28 01:52:33 +0000

    fix

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'luistelmocosta@gmail.com';  
 
     $email_template = 'simple.html';
 
