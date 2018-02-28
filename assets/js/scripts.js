b9f81da7e8fccd8e460350b3a55a2f097ad28312
LuísCosta <luistelmocosta@gmail.com>
2018-02-28 01:33:37 +0000

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(41.206239, -8.336768);
 
             var styles = [
                 {
